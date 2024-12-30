522ef51ea6c59fc66f90a1aba0a10d50f0889c9a
tenzu <huangxin.cliff@gmail.com>
2024-12-30 22:20:02 +0800

    9_1 updated

--- chapter9/9_1.py
+++ chapter9/9_1.py
@@ -1,10 +1,19 @@
 import chapter9module as c9m
 
-a = 0.2
-b = 0.06
+a = 2
+b = 0.6
 t = 0.012
-q = 6.5 / 0.01
-D = c9m.cylindricalStiffness(t)
-print(D)
-Mb=c9m.bendingMomentFixedEnd(q,b)
+q = 6.5 / 0.01 / 0.01
+
+print(f"计算A点弯矩：")
+Ma = c9m.bendingMomentFixedEnd1(q, b)
+print(Ma)
+print(f"计算A点由弯曲引起的正应力：")
+sigma_a = c9m.bendingNormalStress(Ma, t)
+print(sigma_a)
+print(f"计算A点挠度：")
+v_a = c9m.midSpanDeflection1(q, b, c9m.E1(2e11, 0.3), t)
+print(v_a)
+print(f"计算B点弯矩：")
+Mb = c9m.bendingMomentFixedEnd2(q, a)
 print(Mb)
--- chapter9/chapter9module.py
+++ chapter9/chapter9module.py
@@ -2,10 +2,32 @@
 youngsModulus = 2e11
 poissonsRatio = 0.3
 
-# cylindrical stiffness
-def cylindricalStiffness(plateThickness):
-    return youngsModulus*plateThickness**3/(12*(1-poissonsRatio**2))
+# E1
+def E1(youngsModulus=2e11, poissonsRatio=0.3):
+    return youngsModulus/(2*(1-poissonsRatio**2))
 
-# bending moment at fixed end
-def bendingMomentFixedEnd(distributedLoad, beamWidth):
-    return distributedLoad*beamWidth**2/12
+# 板条梁截面惯性矩
+def I1(plateThickness):
+    return plateThickness**3/12
+
+# 筒形刚度
+# def cylindricalStiffness(plateThickness):
+#     return youngsModulus*plateThickness**3/(12*(1-poissonsRatio**2))
+def cylindricalStiffness(plateThickness):
+    return E1(youngsModulus, poissonsRatio)*plateThickness**3/12
+
+# 两端固支梁跨中弯矩
+def bendingMomentFixedEnd1(distributedLoad, span):
+    return distributedLoad*span**2/24
+
+# 两端固支梁固端弯矩
+def bendingMomentFixedEnd2(distributedLoad, span):
+    return distributedLoad*span**2/12
+
+# 普通梁的弯曲正应力
+def bendingNormalStress(moment, plateThickness):
+    return 6*moment/(plateThickness**2)
+
+# 两端固支梁跨中挠度
+def midSpanDeflection1(distributedLoad, span, E1, plateThickness):
+    return distributedLoad*span**4/(384*E1*I1(plateThickness))
