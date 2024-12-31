153f58289ebc51159d2921a2a69bc5028f1d0854
Tenzu <huangxin.cliff@gmail.com>
2024-12-31 10:42:15 +0800

    9_1 finished

--- chapter9/9_1.py
+++ chapter9/9_1.py
@@ -4,16 +4,48 @@ a = 2
 b = 0.6
 t = 0.012
 q = 6.5 / 0.01 / 0.01
+sigma_0 = 18.8e6
 
+print(f'求解（a)')
 print(f"计算A点弯矩：")
-Ma = c9m.bendingMomentFixedEnd1(q, b)
-print(Ma)
+M_A = c9m.bendingMomentFixedEnd1(q, b)
+print(f'M_A = {M_A :.2f} N*m\n')
 print(f"计算A点由弯曲引起的正应力：")
-sigma_a = c9m.bendingNormalStress(Ma, t)
-print(sigma_a)
-print(f"计算A点挠度：")
-v_a = c9m.midSpanDeflection1(q, b, c9m.E1(2e11, 0.3), t)
-print(v_a)
+sigma_A = c9m.bendingNormalStress(M_A, t)
+print(f'sigma_A = {sigma_A :.2f} Pa = {sigma_A*1e-6 :.2f} MPa\n')
+
 print(f"计算B点弯矩：")
-Mb = c9m.bendingMomentFixedEnd2(q, a)
-print(Mb)
+M_B = c9m.bendingMomentFixedEnd2(q, b)
+print(f'M_B = {M_B :.2f} N*m\n')
+print(f'计算B点由弯曲引起的正应力：')
+sigma_B = c9m.bendingNormalStress(M_B, t)
+print(f'sigma_B = {sigma_B :.2f} Pa = {sigma_B*1e-6 :.2f} MPa\n')
+
+print(f'计算A点挠度：')
+v_A = c9m.midSpanDeflection1(q,b,c9m.E1(2e11,0.3),t)
+print(f'v_A = {v_A :.4f} m = {v_A*1e3 :.3f} mm\n')
+
+print(f'求解（b)')
+print(f'计算板条梁的中面拉力：')
+T = 18.8e6*1*t
+print(f'T = {T :.2f} N\n')
+print(f'计算 u 的值：')
+u = c9m.u(b, T, c9m.cylindricalStiffness(t))
+print(f'u = {u :.3f}\n')
+print(f'计算A点复杂弯曲弯矩：')
+M_A_complex = M_A*c9m.phai1(u)
+print(f'M_A_complex = {M_A_complex :.2f} N*m\n')
+print(f'计算A点由复杂弯曲引起的正应力：')
+sigma_A_complex = c9m.bendingNormalStress(M_A_complex, t)
+print(f'sigma_A_complex = {sigma_0+sigma_A_complex :.2f} Pa = {(sigma_0+sigma_A_complex)*1e-6 :.2f} MPa\n')
+
+print(f'计算A点复杂弯曲挠度：')
+v_A_complex = v_A*c9m.f1(u)
+print(f'v_A_complex = {v_A_complex :.4f} m = {v_A_complex*1e3 :.3f} mm\n')
+
+print(f'计算B点复杂弯曲弯矩：')
+M_B_complex = c9m.bendingMomentFixedEnd2(q, b)*c9m.X(u)
+print(f'M_B_complex = {M_B_complex :.2f} N*m\n')
+print(f'计算B点由复杂弯曲引起的正应力：')
+sigma_B_complex = c9m.bendingNormalStress(M_B_complex, t)
+print(f'sigma_B_complex = {sigma_0+sigma_B_complex :.2f} Pa = {(sigma_0+sigma_B_complex)*1e-6 :.2f} MPa\n')
--- chapter9/chapter9module.py
+++ chapter9/chapter9module.py
@@ -1,33 +1,59 @@
 # This is a module for chapter 9 exercises.
+from math import *
+
 youngsModulus = 2e11
 poissonsRatio = 0.3
 
+
 # E1
 def E1(youngsModulus=2e11, poissonsRatio=0.3):
-    return youngsModulus/(2*(1-poissonsRatio**2))
+    return youngsModulus / (1 - poissonsRatio**2)
+
 
 # 板条梁截面惯性矩
 def I1(plateThickness):
-    return plateThickness**3/12
+    return plateThickness**3 / 12
+
 
 # 筒形刚度
 # def cylindricalStiffness(plateThickness):
 #     return youngsModulus*plateThickness**3/(12*(1-poissonsRatio**2))
 def cylindricalStiffness(plateThickness):
-    return E1(youngsModulus, poissonsRatio)*plateThickness**3/12
+    return E1(youngsModulus, poissonsRatio) * plateThickness**3 / 12
+
 
 # 两端固支梁跨中弯矩
 def bendingMomentFixedEnd1(distributedLoad, span):
-    return distributedLoad*span**2/24
+    return distributedLoad * span**2 / 24
+
 
 # 两端固支梁固端弯矩
 def bendingMomentFixedEnd2(distributedLoad, span):
-    return distributedLoad*span**2/12
+    return distributedLoad * span**2 / 12
+
 
 # 普通梁的弯曲正应力
 def bendingNormalStress(moment, plateThickness):
-    return 6*moment/(plateThickness**2)
+    return 6 * moment / (plateThickness**2)
+
 
 # 两端固支梁跨中挠度
 def midSpanDeflection1(distributedLoad, span, E1, plateThickness):
-    return distributedLoad*span**4/(384*E1*I1(plateThickness))
+    return distributedLoad * span**4 / (384 * E1 * I1(plateThickness))
+    # return distributedLoad*span**4/(384*cylindricalStiffness(plateThickness))
+
+# u 的值
+def u(span, inplaneForce, cylindricalStiffness):
+    return span/2*sqrt(inplaneForce/cylindricalStiffness)
+
+# 拉力弯矩辅助函数1
+def phai1(u):
+    return (6/u**2)*(1-u/sinh(u))
+
+# 拉力弯矩辅助函数2
+def X(u):
+    return (3/u**2)*(u/tanh(u)-1)
+
+# 拉力挠度辅助函数
+def f1(u):
+    return 24/u**3*(u/2-tanh(u/2))
