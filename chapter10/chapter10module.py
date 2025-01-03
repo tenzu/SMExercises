26d86b863d4880c868b9a04d86ecfee0b9259b5c
Tenzu <huangxin.cliff@gmail.com>
2025-01-03 11:45:40 +0800

    10_1 updated

---
+++ chapter10/chapter10module.py
@@ -0,0 +1,17 @@
+# This is a module for chapter 10 exercises.
+
+
+youngsModulus = 2e11
+poissonsRatio = 0.3
+
+
+# 形心计算公式
+def centroidofAreas(A1, y1, A2, y2):
+    y0 = (A1 * y1 + A2 * y2) / (A1 + A2)
+    return y0
+
+
+# 材料力学移轴公式
+def shiftAxisFormula(I1, A1, d1, I2, A2, d2):
+    I0 = I1 + I2 + A1 * d1**2 + A2 * d2**2
+    return I0
