492f383825c1017eda435a4979a5bd0a26654fe5
Tenzu <huangxin.cliff@gmail.com>
2025-01-03 13:34:44 +0800

    10_1 updated

--- chapter10/chapter10module.py
+++ chapter10/chapter10module.py
@@ -1,5 +1,5 @@
 # This is a module for chapter 10 exercises.
-
+from math import pi
 
 youngsModulus = 2e11
 poissonsRatio = 0.3
@@ -15,3 +15,8 @@ def centroidofAreas(A1, y1, A2, y2):
 def shiftAxisFormula(I1, A1, d1, I2, A2, d2):
     I0 = I1 + I2 + A1 * d1**2 + A2 * d2**2
     return I0
+
+
+# 单跨梁欧拉力公式
+def eularForceSingleBeam(I, L,E =youngsModulus):
+    return pi**2*E*I/L**2
