38db8add84cca9b98391b0c29fd240cdac538d13
Tenzu <huangxin.cliff@gmail.com>
2025-01-08 11:46:20 +0800

    modified:   chapter9/9.9.py

--- chapter9/9.9.py
+++ chapter9/9.9.py
@@ -11,3 +11,7 @@ A11 = sp.symbols('A11', real=True)
 w = A11* sp.sin(sp.pi*x/a)*sp.sin(sp.pi*y/(4*b))
 
 print(f"---- 写出边界条件 ----")
+print(f"x = 0 时：w = {w.subs(x,0)}， w' = {w.diff(x).subs(x,0)}， w'' = {w.diff(x,2).subs(x,0)}")
+print(f"x = a 时：w = {w.subs(x,a)}， w' = {w.diff(x).subs(x,a)}， w'' = {w.diff(x,2).subs(x,a)}")
+print(f"y = 0 时：w = {w.subs(y,0)}， w' = {w.diff(y).subs(y,0)}， w'' = {w.diff(y,2).subs(y,0)}")
+print(f"y = a 时：w = {w.subs(y,b)}， w' = {w.diff(y).subs(y,b)}， w'' = {w.diff(y,2).subs(y,b)}")
