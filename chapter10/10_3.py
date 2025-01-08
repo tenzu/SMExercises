a5551d5b69bfdd57b945d5ab0c6909046b9c97af
Tenzu <huangxin.cliff@gmail.com>
2025-01-08 10:15:12 +0800

    10_3 finished

--- chapter10/10_3.py
+++ chapter10/10_3.py
@@ -9,6 +9,7 @@ l = sp.Symbol("l", positive=True)
 v = a1 * x * (l - x)
 E = sp.Symbol("E", positive=True)
 I0 = sp.Symbol("I0", positive=True)
+T = sp.Symbol("T", positive=True) # 本章默认杆件轴向压力为正
 # x = 0时I1=0.4*I0,x=0.2*l时I1=I0
 I1 = (3 * I0 / l) * x + 0.4 * I0
 I2 = I0
@@ -19,4 +20,15 @@ I3 = (-3 * I0 / l) * x + 3.4 * I0
 V1 = c10m.beamBendingEnergy(v, I1, 0, 0.2 * l, E)
 V2 = c10m.beamBendingEnergy(v, I2, 0.2 * l, 0.8 * l, E)
 V3 = c10m.beamBendingEnergy(v, I3, 0.8 * l, 1.0 * l, E)
-print(f"杆件弯曲应变能：{V1+V2+V3}")
+V = V1 + V2 + V3
+print(f"杆件弯曲应变能：{V}")
+
+# 计算杆件力函数
+U = 1/2 * sp.integrate(T*sp.diff(v,x)**2, (x, 0, l))
+print(f"杆件力函数：{U}")
+
+# 另总位能对系数求偏导等于零
+PI = V - U
+equation = sp.Eq(sp.diff(PI,a1),0)
+T = sp.solve(equation,T)
+print(f"杆件弯曲应变系数：{T}")
