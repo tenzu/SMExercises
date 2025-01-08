79906e3b11755fc65608e71ccede84142a0ebba1
Tenzu <huangxin.cliff@gmail.com>
2025-01-08 11:37:28 +0800

    modified:   chapter9/9.9.py

--- chapter9/9.9.py
+++ chapter9/9.9.py
@@ -1,4 +1,13 @@
 import chapter9module as c9m
+import sympy as sp
 
 print(f"********** 9.9题 **********")
 
+a,b = sp.symbols('a b', positive=True)
+x, y = sp.symbols('x y')
+E_beam, I_beam = sp.symbols('E_beam I_beam', positive=True)
+# 挠曲面函数仅取第一项
+A11 = sp.symbols('A11', real=True)
+w = A11* sp.sin(sp.pi*x/a)*sp.sin(sp.pi*y/(4*b))
+
+print(f"---- 写出边界条件 ----")
